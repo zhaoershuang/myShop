d185b986f0ca5af570eb354129fe0c07cb7fa1a8
zhaoershuang <1280478320@qq.com>
2018-01-12 11:43:21 +0800

    my shop

---
+++ pages/main/main.js
@@ -0,0 +1,12 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000
+  }
+})
